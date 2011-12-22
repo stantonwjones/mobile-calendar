7e9c8123b8e7036189dcd0a62997861b8d2f47f8
Stanton Jones <stanton.jones@moovweb.com>
2011-12-22 11:37:55 -0800

    debugging mobile calendar

--- mobile-calendar.js
+++ mobile-calendar.js
@@ -1,10 +1,12 @@
 //  Requires xui.js
-//x$(document).on('DOMContentLoaded') {
-//}
+x$(document).on('DOMContentLoaded', function() {
+  initMobCal();
+});
+var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 function insertCalendar() {
   var months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-  var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
   x$('body').bottom(''+
     "<div id='mobileCalendarWrapper'>"+
@@ -33,6 +35,7 @@ function insertCalendar() {
         "</div>"+
         "<div id='moileCalendar:closeButtonWrapper'>"+
           "<span id='mobileCalendar:button-close'>"+
+          "X"+
           "</span>"+
         "</div>"+
       "</div>"+
@@ -49,21 +52,126 @@ function insertCalendar() {
         "</div>"+
         "<div id='mobileCalendar:days-row-5'>"+
         "</div>"+
+        "<div id='mobileCalendar:days-row-6'>"+
+        "</div>"+
       "</div>"+
     "</div>"+
   "");
 
-  function insertDays(element, index, xui) {
-    console.log(days);
-    for(day in days) {
-      element.innerHTML+="<span class='" + days[day] + " mobileCalendar:dayCell'></span>";
-    }
-  }
   function insertDayHeaders() {
     for(day in days) {
-      x$('#mobileCalendar\\:days-header').bottom("<span class='" + days[day] + "-header mobileCalendar:dayHeaderCell'>" + days[day] + "</span>");
+      x$('#mobileCalendar\\:days-header').bottom("<span class='" + days[day] + "-header mobileCalendar:dayHeaderCell'>" + days[day][0] + "</span>");
     }
   }
-  x$('[id|="mobileCalendar:days-row"]').each(insertDays);
   insertDayHeaders();
+}
+
+function initMobCal(reset) {
+  var now = new Date();
+  var calDate = new Date();
+  calDate.setDate(1);
+  var maxDays = [31, 28, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31];
+  window.mobCal = {'now':now, 'calDate':calDate, 'maxDays':maxDays};
+  populateCalendar();
+  setButtons();
+  console.log(reset);
+  if (!Boolean(reset)) {
+    insertCalendar();
+  }
+}
+
+function formatNumberLength(num) {
+  var length=2;
+  var r = "" + num;
+  while (r.length < length) {
+    r = "0" + r;
+  }
+  return r;
+}
+
+function incrementYear() {
+  window.mobCal.calDate.setFullYear(parseInt(window.mobCal.calDate.getFullYear()) + 1);
+  populateCalendar();
+}
+function decrementYear() {
+  window.mobCal.calDate.setFullYear(parseInt(window.mobCal.calDate.getFullYear()) - 1);
+  populateCalendar();
+}
+function incrementMonth() {
+  if (window.mobCal.calDate.getMonth() < 11) {
+    window.mobCal.calDate.setMonth(window.mobCal.calDate.getMonth() + 1);
+    populateCalendar();
+  } else {
+    window.mobCal.calDate.setMonth(0);
+    incrementYear();
+  }
+}
+function decrementMonth() {
+  if (window.mobCal.calDate.getMonth() > 0) {
+    window.mobCal.calDate.setMonth(window.mobCal.calDate.getMonth() - 1);
+    populateCalendar();
+  } else {
+    window.mobCal.calDate.setMonth(11);
+    decrementYear();
+  }
+}
+
+function populateCalendar() {
+  var offset = window.mobCal.calDate.getDay();
+  function insertDays(element, index, xui) {
+    for(day in days) {
+      element.innerHTML+=("<span class='" + days[day] + " mobileCalendar:dayCell'>" + (1 + parseInt(index)*7 + parseInt(day)) + "</span>");
+    }
+  }
+  x$('[id|="mobileCalendar:days-row"]').inner('');
+  x$('[id|="mobileCalendar:days-row"]').each(insertDays);
+  function setDay(element, index, xui) {
+    var dayOfMonth = parseInt(element.innerHTML);
+    console.log(dayOfMonth);
+    dayOfMonth-=offset;
+    console.log(dayOfMonth);
+    if (dayOfMonth < 1 || dayOfMonth > window.mobCal.maxDays[window.mobCal.calDate.getMonth()]) {
+      element.innerHTML='';
+    } else {
+      element.innerHTML=dayOfMonth;
+    }
+  }
+  x$('[class*="mobileCalendar:dayCell"]').each(setDay);
+  x$('[id="mobileCalendar:monthWrapper"]').each(function(element, index, xui){element.innerHTML=months[window.mobCal.calDate.getMonth()];});
+  x$('[id="mobileCalendar:yearWrapper"]').each(function(element, index, xui){element.innerHTML=window.mobCal.calDate.getFullYear();});
+}
+
+function setButtons() {
+  x$('#mobileCalendar\\:button-yearEarlier').click(decrementYear);
+  x$('#mobileCalendar\\:button-yearLater').click(incrementYear);
+  x$('#mobileCalendar\\:button-monthEarlier').click(decrementMonth);
+  x$('#mobileCalendar\\:button-monthLater').click(incrementMonth);
+  x$('#mobileCalendar\\:button-close').click(closeCalendar);
+  x$('.mobileCalendar\\:dayCell').click(setInputValue);
+  x$('[data-ur-mobileCalendarInput="true"]').each(openCalendar);
+}
+
+function closeCalendar() {
+  //reset values and set display to none
+}
+
+function setInputValue() {
+  var inp = window.mobCal.selectedInput;
+  inp.setAttribute('value', getDateString(this));
+  closeCalendar();
+}
+
+function openCalendar(element, index, xui) {
+  window.mobCal.selectedInput = element;
+  resetCalendar();
+  //show Calendar, enable input field if it is disabled
+}
+
+function resetCalendar() {
+  initMobCal(true);
+}
+
+function getDateString(dayCell) {
+  var dayNum = parseInt(dayCell.innerHTML);
+  return (formatNumberLength(dayNum) + '/' + formatNumberLength(window.mobCal.calDate.getMonth() + 1) + '/' + window.mobCal.calDate.getFullYear());
 }
